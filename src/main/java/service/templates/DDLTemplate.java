f5dc57fa663aad73d60365abacc42acba7e13d60
dolofinskyi <132900577+dolofinskyi@users.noreply.github.com>
2023-10-12 18:31:00 +0300

    Update DDLTemplate.java

--- src/main/java/service/templates/DDLTemplate.java
+++ src/main/java/service/templates/DDLTemplate.java
@@ -2,11 +2,11 @@ package service.templates;
 
 public class DDLTemplate {
     public static final String ADD_WORKER = "INSERT INTO worker(NAME, BIRTHDAY, LEVEL, SALARY) " +
-            "VALUES (?, ?, ?, ?);";
+            "VALUES (?, ?, ?, ?)";
     public static final String ADD_CLIENT = "INSERT INTO client(NAME) " +
-            "VALUES (?);";
+            "VALUES (?)";
     public static final String ADD_PROJECT = "INSERT INTO project(CLIENT_ID, START_DATE, FINISH_DATE) " +
-            "VALUES(?, ?, ?);";
+            "VALUES(?, ?, ?)";
     public static final String ADD_PROJECT_WORKER = "INSERT INTO project_worker(PROJECT_ID, WORKER_ID) " +
-            "VALUES(?, ?);";
+            "VALUES(?, ?)";
 }
